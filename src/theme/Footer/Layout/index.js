bd25ef1fe92f1cffa3e05a4a7ce8a1df54752182
danielwang <danielwang130@gmail.com>
2022-10-25 10:52:00 +1100

    feat: use swizzle to customise footer

--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import clsx from 'clsx';
+import styles from './footer.module.scss';
 export default function FooterLayout({style, links, logo, copyright}) {
   return (
     <footer
       className={clsx('footer', {
         'footer--dark': style === 'dark',
-      })}>
-      <div className="container container-fluid">
+        }, styles.footer)}>
+
+        {logo && <div className="margin-bottom--sm">{logo}</div>}
         {links}
-        {(logo || copyright) && (
-          <div className="footer__bottom text--center">
-            {logo && <div className="margin-bottom--sm">{logo}</div>}
-            {copyright}
-          </div>
-        )}
-      </div>
+        {copyright}
     </footer>
   );
 }
